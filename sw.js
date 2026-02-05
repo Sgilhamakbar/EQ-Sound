7629f94958658b5ff32585265b08f1ef3e2f819a
Sgilhamakbar <100102412+Sgilhamakbar@users.noreply.github.com>
2026-02-06 03:54:32 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'audio-studio-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// Install Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Menyimpan aset ke cache...');
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Activate & Hapus Cache Lama
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          console.log('Menghapus cache lama:', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
+
+// Fetch (Gunakan Cache jika offline)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
